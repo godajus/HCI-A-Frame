ebe22cf48e39964dc0f14f2c64c2f408817f47da
godajus <45212533+godajus@users.noreply.github.com>
2021-10-21 02:42:58 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -6,7 +6,7 @@ var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
 myOtherBox.object3D.rotation.y -= rotationSpeed;
-console.log(myOtherBox.object3D.rotation.x);
+console.log(myOtherBox.object3D.rotation.y);
  }
 
  setInterval(spin, 16); //equivalent to 60 fps
